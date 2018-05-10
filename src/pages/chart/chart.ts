7c89c1761fe2dc9bf5188ac5f98f45a92fa482fc
timothyrmallett <timothyrmallett@gmail.com>
2018-05-10 19:45:31 +1000

    finished lab 9

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -11,15 +11,21 @@ import { Chart } from 'chart.js';
 })
 export class ChartPage {
 
+  public chartNum: number;
+  public labelNum: number;
+
 	@ViewChild('barChartCanvas')barCanvas;
 	barChart: any;
-
+/*
 	public chartData:Array<any> = [
 	  {data: [10, 15, 33, 37, 42, 51, 60, 61, 63, 57], label: 'Correct Notes'},
 	  {data: [26, 23, 14, 17, 13, 11, 6, 8, 11, 4, 5], label: 'Incorrect Notes'}
 	];
+*/
 
-	public chartLabels:Array<any> = ['1', '2', '3', '4', '5', '6', '7', '8','9','10'];
+  public chartData = [10, 15, 33, 37, 42];
+
+	public chartLabels = ['1', '2', '3', '4', '5'];
 
 	public chartType:string = 'line';
 
@@ -28,19 +34,27 @@ export class ChartPage {
   }
 
   ionViewDidLoad() {
+    this.labelNum = this.chartLabels.length;
     this.barChart = new Chart(this.barCanvas.nativeElement, {
     	type: 'bar',
     	data: {
-    		labels: ["1","2","3","4","5"],
+    		labels: this.chartLabels,
     		datasets:[{
-    			label: '# of Votes',
-    			data: [10, 15, 33, 37, 42],
+    			label: '# of Numbers lol',
+    			data: this.chartData,
     			backgroundColor: [
     				'rgba(255, 17, 100, 0.3)'
     			]
     		}]
     	}
     });
+  }
+
+  addToChart(){
+    //this.chartData.push(this.chartNum);
+    this.labelNum++;
+    this.chartLabels.push(this.labelNum.toString());
+    this.barChart.update();
   }
 
 }
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+declare var google;
 /**
  * Generated class for the MapPage page.
  *
@@ -15,11 +15,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
+	@ViewChild('map')mapEl;
+	map: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+    let latLng = new google.maps.LatLng(-27.962349, 153.380788);
+    let mapOptions = {
+    					center:latLng,
+    					zoom: 16,
+    					mapTypeId: google.maps.MapTypeId.ROADMAP
+    				}
+    this.map = new google.maps.Map(this.mapEl.nativeElement, mapOptions);
+
+    let marker = new google.maps.Marker({
+    	map:this.map,
+    	animation: google.maps.Animation.DROP,
+    	position:this.map.getCenter()
+    });
   }
 
 }
